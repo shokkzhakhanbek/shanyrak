56f86510f4e79f0cbdc76d11791a0212fc53cf0c
shokkzhakhanbek <makhkambayev.s04@gmail.com>
2026-03-23 00:05:40 +0800

    fix: with table creation

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.auth.router import router as auth_router
 from app.shanyraks.router import router as shanyraks_router
 from app.comments.router import router as comments_router
 
-# Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Şañıraq.kz", description="MVP маркетплейса жилых помещений")
 
