a2ca92b9b7e0749713ead5f6f9024b99a1e655bc
shokkzhakhanbek <makhkambayev.s04@gmail.com>
2026-03-22 23:49:41 +0800

    add alembic migration for all tables

---
+++ alembic/versions/74664e67d2b6_create_users_shanyraks_comments_tables.py
@@ -0,0 +1,24 @@
+"""create users shanyraks comments tables
+
+Revision ID: 74664e67d2b6
+Revises: 
+Create Date: 2026-03-22 23:47:49.931951
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = '74664e67d2b6'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
